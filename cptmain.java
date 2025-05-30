7df0c4e487519ea625f2c81a7df4b71967ab1be8
vlad-source-code <David.Costea26@ycdsbk12.ca>
2025-05-30 10:56:54 -0400

    added the options to the console and the option to choose one of them, also have started making the themes show when choosing play game

---
+++ cptmain.java
@@ -0,0 +1,22 @@
+import arc.*;
+
+public class cptmain{
+
+public static void main (String[] args){
+		
+		int intChoice = 0;
+		
+		// when you start, see a logo, play game, view leaderboard, add theme and quit
+		while(intChoice != 1){
+		con.println("Word Guessing Game!");
+		con.println("1: Play Game");
+		con.println("2: View LeaderBoard");
+		con.println("3: Add Theme");
+		con.println("4: Quit");
+		con.println("Choose a option");
+		intChoice = con.readInt();
+		
+		if(intChoice 
+		}
+	}
+}
