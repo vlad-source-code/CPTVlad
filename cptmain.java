7aa915a6989b1014263996eb2f5ad54ed36d72f9
vlad-source-code <David.Costea26@ycdsbk12.ca>
2025-06-01 23:03:57 -0400

    added where the point system matches with the word length and how if you put a letter that's in it, shows all matching spots

--- cptmain.java
+++ cptmain.java
@@ -29,8 +29,8 @@ public static void main (String[] args){
 				String strTheme;
 				strTheme = con.readLine();
 				// depending on the theme they enter, open the respective txt file, then select a random word from that file and print to console using ---
-				if(strTheme.contains("Foods")){
-					TextInputFile Foods = new TextInputFile("Foods.txt");
+				if(strTheme.contains("foods")){
+					TextInputFile Foods = new TextInputFile("foods.txt");
 					int intFoodsNo = 0;
 					while(Foods.eof() == false){
 							Foods.readLine();
@@ -39,10 +39,10 @@ public static void main (String[] args){
 					Foods.close();
 					// select a random number from 0 to the number of fodd items and use that in the food file with the corsoponding word
 					 int randomNum = (int)(Math.random() * (intFoodsNo));
-					 con.println(randomNum);
+					 //con.println(randomNum);
 					String strFoods[] = new String[intFoodsNo];
 					int intIdx = 0;
-					Foods = new TextInputFile("Foods.txt");
+					Foods = new TextInputFile("foods.txt");
 					while(Foods.eof() == false){
 							strFoods[intIdx]=Foods.readLine();
 							intIdx = intIdx +1;
@@ -55,8 +55,33 @@ public static void main (String[] args){
 					for(intcount=0;intcount<strHiddenWord.length();intcount++){
 					   strDashedWord.setCharAt(intcount,'-');
 					}
+					//testing
 					//con.println("The random word is:"+strHiddenWord);
 					con.println("The dashed version of the random word is:"+strDashedWord);
+					
+					//have the user guess a letter and if correct replace the dashes with the letter (can be more than one spot)
+					//initiatlize points
+					//have a Guess boolean variable which is initialized as false and if a letter is guessed is set to true
+					boolean Guess = false;
+					int intPoints=strHiddenWord.length();
+					char ch=' ';
+					con.println("Please enter your guess for a letter:");
+					ch = con.readChar();
+					//parse strDashedWord letter by letter and compare with letter entered by the user
+					//if match replace dash with letter, may be more than one spot
+					for(intcount=0;intcount<strHiddenWord.length();intcount++) {
+					   if(strHiddenWord.charAt(intcount) == ch) {
+						 Guess = true;
+						 strDashedWord.setCharAt(intcount,ch);
+					   }
+				    } 
+				    if (Guess == false){
+						intPoints=intPoints-1;
+					}
+					//testing
+					//con.println(Guess);
+					con.println(strDashedWord);
+					con.println("You have:"+intPoints);
 				}
 		
 		}
